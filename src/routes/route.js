8f4f3f8333f09e52e0f3c62cbb398e7774a27ab3
AtifPervez <atifpervez0@gmail.com>
2022-07-25 14:41:09 +0530

    project5

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,1 +1,19 @@
-//
+const express=require("express")
+const router=express.Router()
+const userController=require("../controllers/userController")
+
+
+
+
+
+router.post("/register",userController.register)
+
+
+
+
+module.exports=router
+
+
+
+
+
