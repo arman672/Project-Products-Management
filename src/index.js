b4db60a18309b19a22d81f277c0f951dd0d03d42
chetanya-rohilla <chetanyarohilla99@gmail.com>
2022-07-25 12:05:13 +0530

    userModel

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-//
+const express = require("express");
+const bodyParser = require("body-parser");
+const route = require("../src/routes/route");
+const mongoose = require("mongoose");
+const app = express();
+const multer = require('multer')
+
+app.use(bodyParser.json());
+app.use(multer().any());
+
+mongoose
+  .connect(
+    "mongodb+srv://atifpervez:34BmDa5XVvtznQvO@code.8mvlc.mongodb.net/group33Database"
+  )
+  .then(() => console.log("MongoDb is connected"))
+  .catch((err) => console.log(err));
+
+app.use("/", route);
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Express app running on port " + (process.env.PORT || 3000));
+});
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,1 +1,62 @@
-//
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    fname: { 
+        type : String, 
+        required : true
+    },
+    lname: { 
+        type : String, 
+        required : true 
+    },
+    email: { 
+        type : String, 
+        required : true,
+        unique : true
+    },
+    profileImage: { 
+        type : String, 
+        required : true
+    },
+    phone: { 
+        type : String, 
+        required : true,
+        unique : true
+    },
+    password: { 
+        type : String, 
+        required : true
+    },
+    address: {
+        shipping: {
+            street: { 
+                type : String, 
+                required : true
+            },
+            city: { 
+                type : String, 
+                required : true
+            },
+            pincode: { 
+                type : Number, 
+                required : true
+            }
+        },
+        billing: {
+            street: { 
+                type : String, 
+                required : true
+            },
+            city:{ 
+                type : String, 
+                required : true
+            },
+            pincode: { 
+                type : String, 
+                required : true
+            }
+        }
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model('User', userSchema)
